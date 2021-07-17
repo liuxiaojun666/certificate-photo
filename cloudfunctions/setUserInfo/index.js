0f559a3ca013a5d9d65cea8e5debf4180d2ba56d
liuxiaojun <lxj_web@163.com>
2021-07-17 13:13:16 +0800

    'bug'

--- cloudfunctions/setUserInfo/index.js
+++ cloudfunctions/setUserInfo/index.js
@@ -6,11 +6,12 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
 	const wxContext = cloud.getWXContext()
-	
+	const data = Object.assign({}, event.data)
+	delete data._id
 	const db = cloud.database()
 	const res = await db.collection('user').where({
 		openid: event.openid || wxContext.OPENID
-	}).update({ data: event.data })
+	}).update({ data })
 
 	return {
 		res,
