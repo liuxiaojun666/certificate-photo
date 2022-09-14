8e7711c539df68b23a1f7e7cabf529309d0abdf5
liuxiaojun <lxj_web@163.com>
2022-09-14 15:45:35 +0800

    客服消息图片修改

--- cloudfunctions/customerService/index.js
+++ cloudfunctions/customerService/index.js
@@ -54,7 +54,7 @@ async function sendImg (wxContext) {
 		type: 'image',
 		media: {
 			contentType: 'image/jpg',
-			value: await getCloudBuffer('cloud://dev-4iov0.6465-dev-4iov0-1301148496/微信图片_20200606104940.jpg')
+			value: await getCloudBuffer('cloud://dev-4iov0.6465-dev-4iov0-1301148496/WechatIMG199.jpeg')
 		}
 	})
 
