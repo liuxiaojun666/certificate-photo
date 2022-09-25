4ff837b8e748a19ad6c8f965d0597ac27116b7f3
liuxiaojun <lxj_web@163.com>
2022-09-25 16:42:40 +0800

    共享云资源环境

--- cloudfunctions/cloudbase_auth/index.js
+++ cloudfunctions/cloudbase_auth/index.js
@@ -19,11 +19,6 @@ exports.main = async (event, context) => {
   return {
     errCode: 0,
     errMsg: '',
-    auth: JSON.stringify({
-      // 自定义安全规则
-      // 在前端访问资源方数据库、云函数等资源时，资源方可以通过
-      // 安全规则的 `auth.custom` 字段获取此对象的内容做校验，
-      // 像这个示例就是资源方可以在安全规则中通过 `auth.custom.x` 获取
-      x: 1,
-    }),
+    auth: JSON.stringify({})
+  }
 }
