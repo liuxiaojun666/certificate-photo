1bc8c0bafc9fd9466a2249e2feec2510d6f92bad
liuxiaojun <lxj_web@163.com>
2022-10-05 13:22:09 +0800

    定时存图片地址

--- cloudfunctions/timingGetImage/index.js
+++ cloudfunctions/timingGetImage/index.js
@@ -2,6 +2,7 @@
 const request = require('request')
 const cloud = require('wx-server-sdk')
 const imgApi = process.env.IMAGE_API
+const meinvImgApi = process.env.MEINV_IMAGE_API
 
 cloud.init()
 
@@ -10,27 +11,71 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
 
-    const imgUrl = await new Promise((resolve, reject) => {
+  // 获取一张美女图片，不管成功不成功，先存起来
+  getMeinvImgUrl()
 
-        request(imgApi, async function (error, response, body) {
-      
-            body = JSON.parse(body)
-            if (body.code != 200) return reject(body)
-    
-            const { total } = await db.collection('bg-images').where({ imgurl: body.imgurl }).count()
-    
-            if (total > 0) return resolve(body.imgurl)
-    
-            db.collection('bg-images').add({
-                data: { imgurl: body.imgurl, create_time: Date.now() }
-            })
-
-            resolve(body.imgurl)
-        })
-    })
-
+  // 返回一张风景图
+  const imgUrl = await getImgUrl()
 
   return {
     imgurl: imgUrl,
   }
+}
+
+function getImgUrl () {
+  return new Promise((resolve, reject) => {
+
+    request(imgApi, async function (error, response, body) {
+  
+        const data = JSON.parse(body)
+        if (data.code != 200) return reject(data)
+
+        try {
+          await db.collection('images').doc(data.imgurl).get()
+        } catch (error) {
+          db.collection('images').add({
+            data: {
+              _id: data.imgurl,
+              imgurl: data.imgurl,
+              create_time: Date.now(),
+              category: 'fengjing',
+              type: 'm'
+            }
+          })
+        }
+
+        const { total } = await db.collection('bg-images').where({ imgurl: data.imgurl }).count()
+
+        if (total > 0) return resolve(data.imgurl)
+
+        db.collection('bg-images').add({
+            data: { imgurl: data.imgurl, create_time: Date.now() }
+        })
+
+        resolve(data.imgurl)
+    })
+  })
+}
+
+// 获取一张美女图片，存在库里
+function getMeinvImgUrl () {
+  request(meinvImgApi, async function (error, response, body) {
+    const data = JSON.parse(body)
+    if (data.code != 200) return reject(data)
+
+    try {
+      await db.collection('images').doc(data.imgurl).get()
+    } catch (error) {
+      db.collection('images').add({
+        data: {
+          _id: data.imgurl,
+          imgurl: data.imgurl,
+          create_time: Date.now(),
+          category: 'meinv',
+          type: 'm'
+        }
+      })
+    }
+
+  })
 }
